4df72870416b4a64ab43705327691f507036c946
Lakshya <lakshyarawal.lr@gmail.com>
2021-08-05 07:55:36 +0530

    Palindrome_Updated

---
+++ Mathematics/palindrome_numbers.py
@@ -0,0 +1,19 @@
+""" Is the Given Number a palindrome or not """
+import math
+val = int(input("Enter your value: "))
+
+
+def is_palindrome(n):
+    rev = 0
+    temp = n
+    while temp >= 1:
+        ld = temp % 10
+        rev = rev * 10 + ld
+        temp = temp//10
+    return rev == n
+
+
+if is_palindrome(val):
+    print("Its a palindrome")
+else:
+    print("Its not a palindrome")
