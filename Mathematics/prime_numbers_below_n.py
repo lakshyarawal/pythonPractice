b3058ca4afad47e73d80d888e3811ab87e3ba0ee
Lakshya <lakshyarawal.lr@gmail.com>
2021-08-09 19:37:08 +0530

    prime_numbers_below_n.py

---
+++ Mathematics/prime_numbers_below_n.py
@@ -0,0 +1,30 @@
+""" Sieve of Eratosthenes: All Prime Numbers below N """
+import math
+""" Naive Solution:  """
+
+
+def prime_num(a) -> list:
+    list_prime = []
+    return list_prime
+
+
+""" Efficient Solution:  """
+
+
+def prime_num_eff(a) -> list:
+    list_eff_prime = []
+    return list_eff_prime
+
+
+def main():
+    val1 = int(input("Enter your value: "))
+    a = prime_num(val1)
+    b = prime_num_eff(val1)
+    print(a)
+    print(b)
+
+
+# Using the special variable
+# __name__
+if __name__ == "__main__":
+    main()
