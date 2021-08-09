4c9aa4795abd05edca2167896172cc091ae65707
Lakshya <lakshyarawal.lr@gmail.com>
2021-08-09 13:40:45 +0530

    check_prime

---
+++ Mathematics/prime_numbers.py
@@ -0,0 +1,46 @@
+""" Check if the number is prime """
+import math
+""" Naive Solution:  """
+
+
+def is_prime(a) -> int:
+    is_prime_num = False
+    for i in range(2, a):
+        if a % i == 0:
+            is_prime_num = True
+            return is_prime_num
+    return is_prime_num
+
+
+""" Efficient Solution: replace a by int(math.sqrt(a)) as divisors occur in pairs only """
+""" More efficient solution: Check basic divisions like 2 and 3 earlier """
+
+
+def is_prime_eff(a) -> int:
+    is_prime_eff_num = False
+    if a % 2 == 0 or a % 3 == 0:
+        is_prime_eff_num = True
+        return is_prime_eff_num
+    for i in range(5, int(math.sqrt(a)), 6):
+        if a % i == 0 or a % (i+2) == 0:
+            is_prime_eff_num = True
+            return is_prime_eff_num
+    return is_prime_eff_num
+
+
+def main():
+    val1 = int(input("Enter your value: "))
+    if is_prime(val1):
+        print("It is not a prime number")
+    else:
+        print("It is a prime number")
+    if is_prime_eff(val1):
+        print("It is not a prime number")
+    else:
+        print("It is a prime number")
+
+
+# Using the special variable
+# __name__
+if __name__ == "__main__":
+    main()
