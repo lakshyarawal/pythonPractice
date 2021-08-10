41c0ce89b79b32a8fc9dcf4960baea64da39a09d
Lakshya <lakshyarawal.lr@gmail.com>
2021-08-10 15:56:56 +0530

    prime_numbers_below_n.py

--- Mathematics/prime_numbers.py
+++ Mathematics/prime_numbers.py
@@ -4,10 +4,10 @@ import math
 
 
 def is_prime(a) -> int:
-    is_prime_num = False
+    is_prime_num = True
     for i in range(2, a):
         if a % i == 0:
-            is_prime_num = True
+            is_prime_num = False
             return is_prime_num
     return is_prime_num
 
@@ -17,13 +17,15 @@ def is_prime(a) -> int:
 
 
 def is_prime_eff(a) -> int:
-    is_prime_eff_num = False
+    is_prime_eff_num = True
+    if a == 2 or a == 3:
+        return is_prime_eff_num
     if a % 2 == 0 or a % 3 == 0:
-        is_prime_eff_num = True
+        is_prime_eff_num = False
         return is_prime_eff_num
     for i in range(5, int(math.sqrt(a)), 6):
         if a % i == 0 or a % (i+2) == 0:
-            is_prime_eff_num = True
+            is_prime_eff_num = False
             return is_prime_eff_num
     return is_prime_eff_num
 
@@ -31,13 +33,13 @@ def is_prime_eff(a) -> int:
 def main():
     val1 = int(input("Enter your value: "))
     if is_prime(val1):
-        print("It is not a prime number")
-    else:
         print("It is a prime number")
+    else:
+        print("It is not a prime number")
     if is_prime_eff(val1):
-        print("It is not a prime number")
-    else:
         print("It is a prime number")
+    else:
+        print("It is not a prime number")
 
 
 # Using the special variable
--- Mathematics/prime_numbers_below_n.py
+++ Mathematics/prime_numbers_below_n.py
@@ -1,19 +1,44 @@
 """ Sieve of Eratosthenes: All Prime Numbers below N """
 import math
-""" Naive Solution:  """
+""" Naive Solution: Check if each number is prime and then add to list ( O(N * rootN) ) """
+
+
+def is_prime_eff(a) -> int:
+    is_prime_eff_num = True
+    if a == 2 or a == 3:
+        return is_prime_eff_num
+    if a % 2 == 0 or a % 3 == 0:
+        is_prime_eff_num = False
+        return is_prime_eff_num
+    for i in range(5, int(math.sqrt(a)), 6):
+        if a % i == 0 or a % (i+2) == 0:
+            is_prime_eff_num = False
+            return is_prime_eff_num
+    return is_prime_eff_num
 
 
 def prime_num(a) -> list:
     list_prime = []
+    for i in range(2, a+1):
+        if is_prime_eff(i):
+            list_prime.append(i)
     return list_prime
 
 
-""" Efficient Solution:  """
+""" Efficient Solution: Creating a boolean array to identify if the integers are prime """
 
 
 def prime_num_eff(a) -> list:
-    list_eff_prime = []
-    return list_eff_prime
+    list_eff_prime = [True] * (a+1)
+    list_prime_eff = []
+    i = 2
+    while i < a+1:
+        if list_eff_prime[i]:
+            list_prime_eff.append(i)
+            for j in range(i*i, a+1, i):
+                list_eff_prime[j] = False
+        i = i+1
+    return list_prime_eff
 
 
 def main():
