46522fe85132cccf0ef2a880383ea5890a9b5ba4
Lakshya <lakshyarawal.lr@gmail.com>
2021-08-27 13:39:00 +0530

    bubble sort

---
+++ Sorting/selection_sort.py
@@ -0,0 +1,21 @@
+""" Selection Sort Algorithm:"""
+
+
+"""Implementation"""
+
+
+def selection_sort(arr) -> list:
+    n = len(arr)
+    return arr
+
+
+def main():
+    arr_input = [10, 5, 30, 1, 2, 5, 10, 10]
+    a2 = selection_sort(arr_input)
+    print(a2)
+
+
+# Using the special variable
+# __name__
+if __name__ == "__main__":
+    main()
