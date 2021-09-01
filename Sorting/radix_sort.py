cf03595d13a53aabb3f08825cfb6b859418d7885
Lakshya <lakshyarawal.lr@gmail.com>
2021-09-01 17:31:04 +0530

    radix sort

--- Sorting/radix_sort.py
+++ Sorting/radix_sort.py
@@ -1,26 +1,38 @@
-""" Bubble Sort Algorithm:"""
+""" Radix Sort Algorithm: This is an extension of counting sort and is able to sort numbers in a higher range in O(n)
+    liner time. 1-step here is to find the number of digits in the max and make the same number of digits in all """
 
 
 """Implementation"""
 
 
-def bubble_sort(arr) -> list:
+def count_sort(arr, k):
     n = len(arr)
-    for j in range(1, n):
-        swapped = True
-        for i in range(n-j):
-            if arr[i] > arr[i+1]:
-                arr[i], arr[i+1] = arr[i+1], arr[i]
-                swapped = False
-        if swapped:
-            break
+    count = [0] * 10
+    for i in range(n):
+        count[(arr[i]//k) % 10] += 1
+    for i in range(1, 10):
+        count[i] = count[i-1] + count[i]
+    output = [0] * n
+    for i in range(n-1, -1, -1):
+        output[count[(arr[i]//k) % 10] - 1] = arr[i]
+        count[(arr[i]//k) % 10] -= 1
+    for i in range(n):
+        arr[i] = output[i]
+
+
+def radix_sort(arr) -> list:
+    max_ele = max(arr)
+    exp = 1
+    while max_ele//exp > 0:
+        count_sort(arr, exp)
         print(arr)
+        exp *= 10
     return arr
 
 
 def main():
-    arr_input = [10, 5, 30, 1, 2, 5, 10, 10]
-    a2 = bubble_sort(arr_input)
+    arr_input = [319, 212, 6, 8, 100, 50]
+    a2 = radix_sort(arr_input)
     print(a2)
 
 
