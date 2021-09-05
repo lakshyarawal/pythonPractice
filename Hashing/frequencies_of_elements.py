18b6dd592bef04b6f094b63ea61e6af47a2d9500
Lakshya <lakshyarawal.lr@gmail.com>
2021-09-05 21:30:43 +0530

    frequencies_of_elements.py

--- Hashing/frequencies_of_elements.py
+++ Hashing/frequencies_of_elements.py
@@ -1,16 +1,21 @@
 
-""" Counting distinct elements in a given array of numbers """
+""" Counting frequency of  elements in a given array of numbers """
 
 
-def count_distinct(arr) -> int:
-    new_set = set(arr)
-    print(new_set)
-    return len(new_set)
+def frequency_elements(arr) -> int:
+    new_dict = dict()
+    for i in range(len(arr)):
+        if arr[i] in new_dict:
+            new_dict[arr[i]] += 1
+        else:
+            new_dict[arr[i]] = 1
+    print(new_dict)
+    return len(new_dict)
 
 
 def main():
-    arr_input = [15, 12, 13, 12, 13, 13, 18]
-    print(count_distinct(arr_input))
+    arr_input = [10, 12, 10, 15, 10, 20, 12, 12]
+    print(frequency_elements(arr_input))
 
 
 # Using the special variable
