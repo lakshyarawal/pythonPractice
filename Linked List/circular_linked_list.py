d53c137fd831ab345b4cad79ed3837372e88c77b
Lakshya <lakshyarawal.lr@gmail.com>
2021-09-13 17:50:02 +0530

    circular_linked_list and inserting new element

---
+++ Linked List/circular_linked_list.py
@@ -0,0 +1,114 @@
+""" Circular Linked List: """
+
+
+class Node:
+    # Function to initialise the node object
+    def __init__(self, data):
+        self.data = data  # Assign data
+        self.next = None  # Initialize next as null
+
+
+class CircularLinkedList:
+
+    # Function to initialize head
+    def __init__(self):
+        self.head = None
+
+    def print_list(self):
+        if self.head is None:
+            return
+        temp = self.head
+        while True:
+            print(temp.data, end=" -> ")
+            temp = temp.next
+            if temp == self.head:
+                print("")
+                break
+
+
+""" To insert at begin and end in O(1) time we add node next to head and then swap the data with the head. Then mark
+    the new head accordingly"""
+
+
+def insert_begin(c_list, new_node):
+    if c_list.head is None:
+        c_list.head = new_node
+        new_node.next = new_node
+        return
+    temp = c_list.head
+    new_node.next = temp
+    while temp:
+        if temp.next == c_list.head:
+            temp.next = new_node
+            c_list.head = new_node
+            return
+        temp = temp.next
+
+
+def insert_end(c_list, new_node):
+    if c_list.head is None:
+        c_list.head = new_node
+        new_node.next = new_node
+        return
+    temp = c_list.head
+    new_node.next = temp
+    while temp:
+        if temp.next == c_list.head:
+            temp.next = new_node
+            new_node.next = c_list.head
+            return
+        temp = temp.next
+
+
+def delete_head(c_list):
+    if c_list.head is None:
+        return
+    if c_list.head.next is None:
+        c_list.head = None
+        return
+    temp = c_list.head
+    temp.data = temp.next.data
+    temp.next = temp.next.next
+
+
+def delete_k_node(c_list, k):
+    if c_list.head is None:
+        return
+    temp = c_list.head
+    i = 1
+    if i == k:
+        delete_head(c_list)
+        return
+    while i < k and temp != c_list.head:
+        if i+1 == k:
+            temp.next = temp.next.next
+        temp = temp.next
+        i += 1
+
+
+def main():
+    c_list = CircularLinkedList()
+    c_list.head = Node(10)
+    second = Node(20)
+    third = Node(30)
+
+    c_list.head.next = second
+    second.next = third
+    third.next = c_list.head
+    c_list.print_list()
+
+    insert_begin(c_list, Node(78))
+    c_list.print_list()
+
+    insert_end(c_list, Node(97))
+    c_list.print_list()
+
+    delete_head(c_list)
+    c_list.print_list()
+
+    delete_k_node(c_list, 1)
+    c_list.print_list()
+
+
+if __name__ == "__main__":
+    main()
---
+++ Linked List/inserting_in_sorted_list.py
@@ -0,0 +1,63 @@
+""" Inserting an Element in a sorted Linked List so that the linked list remains sorted """
+
+
+class Node:
+    # Function to initialise the node object
+    def __init__(self, data):
+        self.data = data  # Assign data
+        self.next = None  # Initialize next as null
+
+
+class LinkedList:
+
+    # Function to initialize head
+    def __init__(self):
+        self.head = None
+
+    def print_list(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end=" -> ")
+            temp = temp.next
+        print("")
+
+
+def insert_new(l_list, new_node):
+    if l_list.head is None:
+        l_list.head = new_node
+        return
+    temp = l_list.head
+    if new_node.data < temp.data:
+        new_node.next = temp
+        l_list.head = new_node
+    while new_node.data > temp.data:
+        if temp.next is None:
+            temp.next = new_node
+            return
+        if temp.next.data > new_node.data:
+            new_node.next = temp.next
+            temp.next = new_node
+            return
+        temp = temp.next
+
+
+def main():
+    l_list = LinkedList()
+
+    l_list.head = Node(10)
+    second = Node(20)
+    third = Node(30)
+
+    l_list.head.next = second
+    second.next = third  # Link second node with the third node
+
+    l_list.print_list()
+    insert_new(l_list, Node(15))
+    l_list.print_list()
+
+
+
+
+if __name__ == '__main__':
+    # Start with the empty list
+    main()
