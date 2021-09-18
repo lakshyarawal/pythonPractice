706a42e1ccb26ae5739a3476a74aa0833902cc37
Lakshya <lakshyarawal.lr@gmail.com>
2021-09-18 23:58:58 +0530

    get min stack

--- Stack/get_min_stack.py
+++ Stack/get_min_stack.py
@@ -1,5 +1,6 @@
-""" Stack is a data structure following LIFO: Last in First Out: which can be used in python in many ways.
-    Below is a representation using singly Linked List """
+""" Get Min using Stacks:  """
+
+from collections import deque
 
 
 class Node:
@@ -16,7 +17,8 @@ class Stack:
     def __init__(self):
         self.head = Node("head")
         self.size = 0
-        self.min_stack1 = Stack()
+        self.min_stack1 = deque()
+        self.min_el = 0
 
     # String representation of the stack
     def __str__(self):
@@ -28,7 +30,6 @@ class Stack:
         return out[:-2]
 
         # Get the current size of the stack
-
     def get_size(self):
         return self.size
 
@@ -47,31 +48,49 @@ class Stack:
 
     # Push a value into the stack.
     def push(self, value):
+        if self.size == 0:
+            self.min_el = value
+        if value < self.min_el:
+            self.min_stack1.append(value-self.min_el)
+            self.min_el = value
+        if len(self.min_stack1) == 0:
+            self.min_stack1.append(value)
+        if self.min_stack1[len(self.min_stack1)-1] >= value:
+            self.min_stack1.append(value)
         node = Node(value)
         node.next = self.head.next
         self.head.next = node
         self.size += 1
+
+    def get_min(self):
+        print(self.min_stack1[len(self.min_stack1)-1])
 
     # Remove a value from the stack and return.
     def pop(self):
         if self.is_empty():
             raise Exception("Popping from an empty stack")
         remove = self.head.next
+        if remove.value < 0:
+            self.min_el = self.min_el - remove.value
+            remove.value = remove.value + self.min_el
+        if remove.value == self.min_stack1[len(self.min_stack1)-1]:
+            self.min_stack1.pop()
         self.head.next = self.head.next.next
         self.size -= 1
         return remove.value
 
 
 def main():
-    stack = Stack()
-    for i in range(1, 11):
-        stack.push(i)
-    print("Original Stack:", stack)
-
-    for _ in range(1, 6):
-        remove = stack.pop()
-        print("Pop:", remove)
-    print("Stack:", stack)
+    s_i = Stack()
+    s_i.push(20)
+    s_i.push(10)
+    s_i.get_min()
+    s_i.push(5)
+    s_i.get_min()
+    s_i.pop()
+    s_i.get_min()
+    s_i.pop()
+    s_i.get_min()
 
 
 # Using the special variable
