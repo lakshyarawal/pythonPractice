3c8a28e00d502038ec16d397e0432d6715e1409a
Lakshya <lakshyarawal.lr@gmail.com>
2021-10-12 18:24:35 +0530

    insert in BST

--- Binary Search Tree/insert.py
+++ Binary Search Tree/insert.py
@@ -8,41 +8,48 @@ class Node:
         self.right = None
 
 
-def search(root_n, val_in):
+def insert(root_n, val_in):
     if root_n is None:
-        print("Element Not Found")
-        return
-    elif root_n.key == val_in:
-        print("Element Found")
-        return
+        root_n = Node(val_in)
+        return root_n
     elif root_n.key > val_in:
-        search(root_n.left, val_in)
+        root_n.left = insert(root_n.left, val_in)
     else:
-        search(root_n.right, val_in)
-    return
+        root_n.right = insert(root_n.right, val_in)
+    return root_n
 
 
-def search_iter(root_n, val_in):
-    while root_n:
-        if root_n.key == val_in:
-            print("Element Found")
-            return
-        elif root_n.key > val_in:
-            root_n = root_n.left
+def insert_iter(root_n, val_in):
+    if root_n.key == val_in:
+        return root_n
+    temp = Node(val_in)
+    parent = None
+    curr_node = root_n
+    while curr_node:
+        parent = curr_node
+        if curr_node.key < val_in:
+            curr_node = curr_node.right
+        elif curr_node.key > val_in:
+            curr_node = curr_node.left
         else:
-            root_n = root_n.right
-    print("Element Not Found")
-    return
+            return root_n
+    if parent is None:
+        return temp
+    if parent.key < val_in:
+        parent.right = temp
+    else:
+        parent.left = temp
+    return root_n
 
 
 def main():
-    root = Node(10)
+    root = Node(30)
     root.left = Node(20)
-    root.right = Node(30)
-    root.right.left = Node(40)
+    root.right = Node(40)
+    root.right.left = Node(35)
     root.right.right = Node(50)
-    search(root, 50)
-    search_iter(root, 50)
+    n_root = insert(root, 70)
+    p_root = insert_iter(root, 70)
 
 
 if __name__ == "__main__":
