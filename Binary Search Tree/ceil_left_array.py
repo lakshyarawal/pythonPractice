68270b959ede1e0617f502dd574f31caa8f1951e
Lakshya <lakshyarawal.lr@gmail.com>
2021-10-17 02:04:49 +0530

    ceiling in array using self balancing BST

---
+++ Binary Search Tree/ceil_left_array.py
@@ -0,0 +1,24 @@
+""" Self Balancing trees """
+from sortedcontainers import SortedSet
+
+
+def ceil_left(arr_in):
+    n_arr = [-1] * len(arr_in)
+    nums = SortedSet()
+    for i in range(len(arr_in)):
+        if arr_in[i] in nums:
+            n_arr[i] = arr_in[i]
+        else:
+            if nums.bisect_left(arr_in[i]) < len(nums):
+                n_arr[i] = nums[nums.bisect_left(arr_in[i])]
+            nums.add(arr_in[i])
+    return n_arr
+
+
+def main():
+    arr = [6, 18, 4, 5]
+    print(ceil_left(arr))
+
+
+if __name__ == "__main__":
+    main()
